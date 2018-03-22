9d2c6bdbd48dd704bbca0466b2e294b38ddefe50
Sergey Slotin <slotserg@gmail.com>
2018-03-22 17:05:42 +0300

    Refactoring

--- hashmap.cpp
+++ hashmap.cpp
@@ -1,21 +1,22 @@
-const int size = (1<<24);
-int a[size], b[size];
+struct hashmap {
+    const int size = (1<<24);
+    int a[size] = {-1}, b[size];
 
-inline int h(int x){ return (x^179)*7; }
+    inline int h (int x) { return (x^179)*7; }
 
-void h_upd(int x, int y){
-	int t = h(x);
-	int k = t % size;
-	while(a[k] != -1 && a[k] != t) k = (k+1) % size;
-	a[k] = t, b[k] = y; 
-}
+    void add (int x, int y) {
+        int t = h(x);
+        int k = t % size;
+        while (a[k] != -1 && a[k] != t)
+            k = (k+1) % size;
+        a[k] = t, b[k] = y; 
+    }
 
-int h_get(int x, int y){
-	int t = h(x);
-	int k = t % size;
-	while(a[k] != -1){
-		if(a[k] == t) return b[k];
-		k = (k+1) % size;
-	}
-	return 0;
-}
+    int get (int x, int y) {
+        int t = h(x);
+        for (int k = t % size; a[k] != -1; k = (k+1) % size) {
+            if (a[k] == t)
+                return b[k];
+        return -1;
+    }
+};
