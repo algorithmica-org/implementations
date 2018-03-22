5ef671dfb61cacd2a9e68834116de20ff10fb9b6
Sergey Slotin <slotserg@gmail.com>
2018-03-22 17:13:53 +0300

    Bugfix

--- hashmap.cpp
+++ hashmap.cpp
@@ -2,17 +2,17 @@ struct hashmap {
     const int size = (1<<24);
     int a[size] = {-1}, b[size];
 
-    inline int h (int x) { return (x^179)*7; }
+    static inline int h (int x) { return (x^179)*7; }
 
     void add (int x, int y) {
         int k = h(x) % size;
         while (a[k] != -1 && a[k] != x)
             k = (k+1) % size;
-        a[k] = t, b[k] = y; 
+        a[k] = x, b[k] = y; 
     }
 
     int get (int x) {
-        for (int k = h(x) % size; a[k] != -1; k = (k+1) % size) {
+        for (int k = h(x) % size; a[k] != -1; k = (k+1) % size)
             if (a[k] == x)
                 return b[k];
         return -1;
