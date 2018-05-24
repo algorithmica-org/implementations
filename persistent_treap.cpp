17de9a4fc676a902effdbd9229a2780de4f22b6e
Sergey Slotin <slotserg@gmail.com>
2018-05-24 20:00:32 +0300

    treap split

--- persistent_treap.cpp
+++ persistent_treap.cpp
@@ -1,17 +1,22 @@
+// untested and not very memory-efficient
+
 struct Node {
-    int key, prior, size = 1;
+    int key, size = 1;
     Node *l = 0, *r = 0;
-    Node (int _key) { key = _key, prior = rand(); }
+    Node (int _key) { key = _key; }
 };
 
 int size (Node *v) { return v ? v->size : 0; }
 
 void upd (Node *v) { v->size = 1 + size(v->l) + size(v->r); }
 
+Node* copy (Node *v) { return v ? new Node(*v) : 0; }
+
 Node* merge (Node *l, Node *r) {
+    l = copy(l), r = copy(r);
     if (!l) return r;
     if (!r) return l;
-    if (l->prior > r->prior) {
+    if (rand() % (size(l) + size(r)) < size(l)) {
         l->r = merge(l->r, r);
         upd(l);
         return l;
@@ -26,6 +31,7 @@ Node* merge (Node *l, Node *r) {
 typedef pair<Node*, Node*> Pair;
 
 Pair split (Node *p, int x) {
+    p = copy(p);
     if (!p) return {0, 0};
     if (size(p->l) + 1 <= x) {
         Pair q = split(p->r, x-size(p->l)-1);
