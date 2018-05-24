73065db9f882203d7da63048be0b46272c25a522
Sergey Slotin <slotserg@gmail.com>
2018-05-24 15:46:57 +0300

    not sure if it works

--- centroid.cpp
+++ centroid.cpp
@@ -3,36 +3,32 @@ using namespace std;
 
 const int maxn = 1e5;
 vector<int> g[maxn];
-bool b[maxn];
+bool used[maxn];
 int s[maxn];
 
-void sizes(int v, int p) {
+void size (int v, int p = -1) {
     s[v] = 1;
     for (int u : g[v])
-        if (!b[u])
-            sizes(u, v), s[v] += s[u];
+        if (u != p && !used[u])
+            size(u, v), s[v] += s[u];
 }
 
-int centroid(int v, int p, int n) {
-    for (size_t i = 0; i < g[v].size(); i++) {
-        int u = g[v][i];
-        if (s[u] > n/2)
+int centroid (int v, int p, int n) {
+    for (int u : g[v])
+        if (u != p && s[u] > n/2)
             return centroid(u, v, n);
-    }
     return v;
 }
 
-void solve(int v) {
+void solve (int v) {
+    sizes(v);
 
     /* ... */
 
-    b[v] = 1;
-    for (int u : g[v]) {
-        if (!b[u]) {
-            sizes(u, v);
-            solve(centroid(u, v, s[u]/2));
-        }
-    }
+    used[v] = 1;
+    for (int u : g[v])
+        if (!used[u])
+            solve(centroid(u, v, s[u]));
 }
 
 int main () {
